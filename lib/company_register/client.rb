9dbe703b30c72682d18ad891c508cec303fd5908
Artur Beljajev <artur.beljajev@internet.ee>
2019-02-06 15:14:26 +0200

    Update `esindus_v1` API

--- lib/company_register/client.rb
+++ lib/company_register/client.rb
@@ -1,15 +1,38 @@
 module CompanyRegister
+  Company = Struct.new(:registration_number)
+
   class Client
     def initialize(soap_client)
       @soap_client = soap_client
     end
 
-    def companies(citizen_personal_code: nil)
-      @soap_client.call(:esindus_v1, message: { keha: { 'ariregister_kasutajanimi' => CompanyRegister.configuration.username,
-                                                        'ariregister_parool' => CompanyRegister.configuration.password,
-                                                        'fyysilise_isiku_kood' => citizen_personal_code,
-                                                        'fyysilise_isiku_koodi_riik' => 'EST',
-                                                        'keel' => 'eng', } })
+    # @citizen_country_code format [String] ISO 3166-1 alpha-3
+    def representation_rights(citizen_personal_code:, citizen_country_code:)
+      soap_operation = :esindus_v1
+
+      params = { 'fyysilise_isiku_kood' => citizen_personal_code,
+                 'fyysilise_isiku_koodi_riik' => citizen_country_code,
+                 'keel' => 'eng' }
+
+      response = send_request(soap_operation, params)
+      parse_representation_rights_response(response)
+    end
+
+    private
+
+    def send_request(soap_operation, params)
+      @soap_client.call(soap_operation, message: { keha: params.merge(default_request_params) })
+    end
+
+    def default_request_params
+      { 'ariregister_kasutajanimi' => CompanyRegister.configuration.username,
+        'ariregister_parool' => CompanyRegister.configuration.password }
+    end
+
+    def parse_representation_rights_response(response)
+      items = response.body[:esindus_v1_response][:keha][:ettevotjad][:item]
+      items = [items] unless items.kind_of?(Array)
+      items.map { |item| Company.new(item[:ariregistri_kood]) }
     end
   end
 end
--- test/client_test.rb
+++ test/client_test.rb
@@ -1,5 +1,17 @@
 require 'test_helper'
 
+class SOAPResponse
+  def body
+    { :esindus_v1_response => { paring: { ariregister_kasutajanimi: 'john',
+                                          ariregister_parool: 'pwd',
+                                          fyysilise_isiku_kood: '1234',
+                                          fyysilise_isiku_koodi_riik: 'EST',
+                                          keel: 'eng' },
+                                keha: { ettevotjad: { item: [{ ariregistri_kood: '12345', }] } } } }
+
+  end
+end
+
 class CompanyRegisterTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::CompanyRegister::VERSION
@@ -12,18 +24,20 @@ class CompanyRegisterTest < Minitest::Test
     end
 
     soap_client = Minitest::Mock.new
-    soap_client.expect(:call, true, [:esindus_v1, { message: { keha: { 'ariregister_kasutajanimi' => 'john',
-                                                                       'ariregister_parool' => 'pwd',
-                                                                       'fyysilise_isiku_kood' => '1234',
-                                                                       'fyysilise_isiku_koodi_riik' => 'EST',
-                                                                       'keel' => 'eng' } } }])
+    soap_client.expect(:call, SOAPResponse.new, [:esindus_v1, { message: {
+        keha: { 'ariregister_kasutajanimi' => 'john',
+                'ariregister_parool' => 'pwd',
+                'fyysilise_isiku_kood' => '1234',
+                'fyysilise_isiku_koodi_riik' => 'USA',
+                'keel' => 'eng' } } }])
     client = CompanyRegister::Client.new(soap_client)
 
-    companies = client.companies(citizen_personal_code: '1234')
+    companies = client.representation_rights(citizen_personal_code: '1234',
+                                             citizen_country_code: 'USA')
 
     soap_client.verify
     assert_kind_of Array, companies
     assert_kind_of CompanyRegister::Company, companies.first
-    assert_kind_of 'some', companies.first.code
+    assert_equal '12345', companies.first.registration_number
   end
 end
