1cf80ae630e18dd31181674493e417b96259e16d
Artur Beljajev <artur.beljajev@internet.ee>
2019-02-06 15:37:55 +0200

    Fix "simplecov" gem config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,7 @@
-require 'simplecov' if ENV['COVERAGE']
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.start
+end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'company_register'
