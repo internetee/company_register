1058936b9c794848a4c002b2a44bb29bb3702e01
Artur Beljajev <artur.beljajev@internet.ee>
2019-02-14 18:00:56 +0200

    Improve test

--- test/request_test.rb
+++ test/request_test.rb
@@ -1,12 +1,10 @@
 require 'test_helper'
 
-class RequestTest < Minitest::Test
-  def setup
-    CompanyRegister.configure {}
-  end
+class ConcreteRequest < CompanyRegister::Request; end
 
-  def test_caches_by_search_params
-    request = CompanyRegister::Request.new(name: 'john')
+class RequestTest < Minitest::Test
+  def test_cache_key
+    request = ConcreteRequest.new(name: 'john')
     assert_equal ({ name: 'john' }).to_json, request.cache_key
   end
 end
